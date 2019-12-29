6bc216d3a3d10aa82c87d0e4c1bd20c980af9653
Serghei <serghei@vbrinc.ro>
2019-12-29 23:33:41 +0200

    add backend for casinos and deals

--- configurations/settings.py
+++ configurations/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/api/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/api/static/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
