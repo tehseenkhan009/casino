a7960ff4c948bd4eebd207eaf26ef4a90faeaf95
Serghei <serghei@vbrinc.ro>
2019-12-29 23:00:33 +0200

    add backend for casinos and deals

--- configurations/settings.py
+++ configurations/settings.py
@@ -128,7 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static')
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
