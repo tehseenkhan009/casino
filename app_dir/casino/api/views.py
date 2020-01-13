c7429fcd65e38f7203559dbf15d62e9814adc017
Serghei <serghei@vbrinc.ro>
2020-01-13 18:53:54 +0200

    add backend for casinos and deals

--- app_dir/casino/api/views.py
+++ app_dir/casino/api/views.py
@@ -12,7 +12,7 @@ from ...core.pagination import PostLimitOffsetPagination
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[-1].strip()
+        ip = x_forwarded_for.split(',')[0].strip()
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
